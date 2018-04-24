5014a6b4a6051b906eb77d76f22ae0531787d566
paulsteban <paul.cisneros@epn.edu.ec>
2018-04-24 17:56:38 -0500

    Typescript
    01_TypeScripts

---
+++ Typescript/01-Typescript.js
@@ -0,0 +1,45 @@
+var nombre = "Paul";
+nombre = "PEPE";
+// 2 tipos de variables lectura, escritura
+var cedula = '1723123141';
+//cedula = "";// reaciacnar igualara otro valor
+var apellido = "Eguez";
+apellido = String(1); //duck Typing
+apellido = 2;
+var asda;
+var usuario = { nombre: "paul", edad: "22" };
+var Usuario = /** @class */ (function () {
+    function Usuario(mNombre, mEdad) {
+        this.nombre = mNombre;
+        this.edad = mEdad;
+    }
+    Usuario.prototype.imprimirUsuario = function (saludo) {
+        //template stings
+        return saludo + " Mi nimbre es : " + this.nombre + ", y mi edad es: " + this.edad;
+    };
+    return Usuario;
+}());
+var UsuarioDos = /** @class */ (function () {
+    function UsuarioDos(nombre, _edad) {
+        this.nombre = nombre;
+        this._edad = _edad;
+    }
+    Object.defineProperty(UsuarioDos.prototype, "edad", {
+        get: function () {
+            return this._edad;
+        },
+        set: function (nuevaEdad) {
+            //cambiar los datos o transformarlos debugear
+            this._edad = nuevaEdad;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return UsuarioDos;
+}());
+var paul = new Usuario("Paul", 23);
+console.log(paul.imprimirUsuario("hola"));
+var juan = new UsuarioDos("pepe", 43);
+console.log(juan);
+var carlos = { nombre: "Carlos", edad: 18 };
+carlos.nombre;
---
+++ Typescript/01-Typescript.ts
@@ -0,0 +1,52 @@
+let nombre: String = "Paul";
+nombre = "PEPE";
+// 2 tipos de variables lectura, escritura
+const cedula = '1723123141';
+//cedula = "";// reaciacnar igualara otro valor
+var apellido :any = "Eguez" ;
+apellido = String(1); //duck Typing
+apellido = 2;
+var asda;
+
+let usuario = { nombre : "paul", edad:"22"};
+class Usuario{
+    nombre :string;
+    private edad:number;
+    constructor(mNombre:string,mEdad:number){
+        this.nombre = mNombre;
+        this.edad = mEdad;
+    }
+    imprimirUsuario(saludo:String){
+        //template stings
+        return `${saludo} Mi nimbre es : ${this.nombre}, y mi edad es: ${this.edad}`;
+    }
+}
+class UsuarioDos{
+    constructor(public nombre:string, private _edad:number){
+
+    }
+get edad():number{
+        return this._edad;
+}
+set edad(nuevaEdad:number) {
+    //cambiar los datos o transformarlos debugear
+        this._edad = nuevaEdad;
+}
+
+
+}
+let paul = new Usuario("Paul",23);
+console.log(paul.imprimirUsuario("hola"));
+let juan = new UsuarioDos("pepe",43);
+console.log(juan);
+
+interface UsuarioTres {
+    //tipar un objeto
+    nombre:string;
+    edad:number;
+
+
+
+}
+let carlos:UsuarioTres={nombre:"Carlos",edad: 18};
+carlos.nombre;
